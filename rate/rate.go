6cac6e4a7945f545054f38d4c6291c07385a5ba2
mikih <miko5054@gmail.com>
2015-06-20 14:30:06 +0300

    fix test

--- rate/rate.go
+++ rate/rate.go
@@ -19,20 +19,15 @@ type conf struct {
 	Time int64 `yaml:"time"`
 }
 
+type userMD struct {
+	meta []string
+}
+
 func RateHandler(h http.Handler) http.Handler {
 	return rateHandler{h}
 }
 
 func (h rateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	b := checkRate(r.RemoteAddr)
-	if b == false {
-		log.Printf("in the false  ")
-		h.handler.ServeHTTP(w, r)
-	}
-
-}
-
-func checkRate(remotAdd string) bool {
 
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
@@ -41,30 +36,77 @@ func checkRate(remotAdd string) bool {
 	})
 	defer client.Close()
 
+	//in a real world case ill will probly  use r to ger user data like app engine
+	u := &userMD{}
+	u.meta = []string{"aaaa", "10.10.110.5"}
+
+	b := checkRateOrBlock(client, r.RemoteAddr, u.meta)
+
+	if b == false {
+		w.WriteHeader(403)
+		w.Write([]byte("DONT abouse"))
+	} else {
+		h.handler.ServeHTTP(w, r)
+
+	}
+
+}
+
+/*
+func (u *userMD) checkUserMD(client *redis.Client, r *http.Request) bool {
+	//in a real world case ill will probly  use r to ger user data like app engine
+	u.meta = []string{"miki", "10.10.10.5"}
+	for _, e := range u.meta {
+		ex, err := client.Exists(e).Result()
+		if err != nil {
+			log.Printf("client.Exists err   #%v ", err)
+		}
+		if ex {
+			log.Printf("key inside the blacklist  therfor blocked")
+			return false
+		}
+	}
+	return true
+}
+*/
+
+func checkRateOrBlock(client *redis.Client, remotAdd string, meta []string) bool {
+
 	c := &conf{}
 	c = c.getConf()
+
+	for _, e := range meta {
+		ex, err := client.Exists(e).Result()
+		if err != nil {
+			log.Printf("client.Exists err   #%v ", err)
+		}
+		if ex {
+			log.Printf("key inside the blacklist  therfor blocked")
+			return false
+		}
+	}
 
 	key := remotAdd + "_" + strconv.FormatInt(time.Now().Unix(), 10)
 
 	//if exsit
 	ex, err := client.Exists(key).Result()
 	if err != nil {
-		log.Printf("client.Get err   #%v ", err)
+		log.Printf("client.Exists err   #%v ", err)
 	}
 
 	if ex {
 
 		i := client.Incr(key)
-		log.Printf("in the else   #%v ", i)
+		log.Printf("key number   #%v ", i)
 		_ = client.Expire(key, time.Millisecond*time.Duration(c.Time))
 
 		if i.Val() > c.Hits {
-			log.Printf("out bbbbbbbbbbbbbbbbbbbbbbbbbbb")
+			log.Printf("over the limit ")
 
 			return false
 		}
 	} else {
-		log.Printf("setttttt")
+		log.Printf("set new key")
 		_ = client.Set(key, "1", time.Minute*time.Duration(c.Time))
 		return true
 	}
--- rate/rate_test.go
+++ rate/rate_test.go
@@ -1,16 +1,60 @@
 package rate
 
 import (
+	"gopkg.in/redis.v3"
 	"testing"
+	"time"
 )
 
 func TestCheckRate(t *testing.T) {
 
-	b := checkRate("10.10.10.5")
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	defer client.Close()
+	u := &userMD{}
+	u.meta = []string{}
+	//case of over  a normal call
+	b := checkRateOrBlock(client, "10.15.10", u.meta)
+
 	if b == false {
 		t.Log("you are over the rate limit ")
+
 	} else {
 		t.Log("not over the limit")
 	}
 
+	//case of over the limit
+	for b != false {
+		b = checkRateOrBlock(client, "10.15.10", u.meta)
+	}
+	if b == false {
+		t.Log("you are over the rate limit ")
+
+	} else {
+		t.Log("not over the limit")
+	}
+}
+
+func TestCheckMd(t *testing.T) {
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	defer client.Close()
+	client.Set("miki", "miki", time.Minute*1)
+	u := &userMD{}
+	u.meta = []string{"miki", "10.10.10.5"}
+	b := checkRateOrBlock(client, "10.15.10", u.meta)
+
+	if b == false {
+		t.Log("key blocked")
+
+	} else {
+		t.Log("key not blocked")
+	}
 }
